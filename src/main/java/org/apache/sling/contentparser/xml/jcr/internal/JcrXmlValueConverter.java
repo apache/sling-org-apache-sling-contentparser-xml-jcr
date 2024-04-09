3b87b0e718eacdce1197bdb4f07e8160d0ec68b1
Stefan Seifert <stefanseifert@users.noreply.github.com>
2024-04-09 10:33:50 +0200

    SLING-12281 apply spotless code formatting

--- src/main/java/org/apache/sling/contentparser/xml/jcr/internal/JcrXmlValueConverter.java
+++ src/main/java/org/apache/sling/contentparser/xml/jcr/internal/JcrXmlValueConverter.java
@@ -1,22 +1,24 @@
-/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
- ~ Licensed to the Apache Software Foundation (ASF) under one
- ~ or more contributor license agreements.  See the NOTICE file
- ~ distributed with this work for additional information
- ~ regarding copyright ownership.  The ASF licenses this file
- ~ to you under the Apache License, Version 2.0 (the
- ~ "License"); you may not use this file except in compliance
- ~ with the License.  You may obtain a copy of the License at
- ~
- ~   http://www.apache.org/licenses/LICENSE-2.0
- ~
- ~ Unless required by applicable law or agreed to in writing,
- ~ software distributed under the License is distributed on an
- ~ "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- ~ KIND, either express or implied.  See the License for the
- ~ specific language governing permissions and limitations
- ~ under the License.
- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
 package org.apache.sling.contentparser.xml.jcr.internal;
+
+import javax.jcr.PropertyType;
 
 import java.lang.reflect.Array;
 import java.math.BigDecimal;
@@ -24,8 +26,6 @@ import java.net.URI;
 import java.net.URISyntaxException;
 import java.util.Calendar;
 import java.util.UUID;
-
-import javax.jcr.PropertyType;
 
 import org.apache.jackrabbit.util.ISO8601;
 import org.apache.jackrabbit.vault.util.DocViewProperty;
@@ -105,7 +105,6 @@ class JcrXmlValueConverter {
                 return null;
             default:
                 throw new IllegalArgumentException("Unexpected type: " + PropertyType.nameFromValue(type));
-
         }
     }
 
@@ -142,8 +141,6 @@ class JcrXmlValueConverter {
                 return null;
             default:
                 throw new IllegalArgumentException("Unexpected type: " + PropertyType.nameFromValue(type));
-
         }
     }
-
 }
--- src/test/java/org/apache/sling/contentparser/xml/jcr/internal/JcrXmlValueConverterTest.java
+++ src/test/java/org/apache/sling/contentparser/xml/jcr/internal/JcrXmlValueConverterTest.java
@@ -1,21 +1,21 @@
-/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
- ~ Licensed to the Apache Software Foundation (ASF) under one
- ~ or more contributor license agreements.  See the NOTICE file
- ~ distributed with this work for additional information
- ~ regarding copyright ownership.  The ASF licenses this file
- ~ to you under the Apache License, Version 2.0 (the
- ~ "License"); you may not use this file except in compliance
- ~ with the License.  You may obtain a copy of the License at
- ~
- ~   http://www.apache.org/licenses/LICENSE-2.0
- ~
- ~ Unless required by applicable law or agreed to in writing,
- ~ software distributed under the License is distributed on an
- ~ "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- ~ KIND, either express or implied.  See the License for the
- ~ specific language governing permissions and limitations
- ~ under the License.
- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
 package org.apache.sling.contentparser.xml.jcr.internal;
 
 import java.math.BigDecimal;
@@ -31,7 +31,7 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNull;
 
 public class JcrXmlValueConverterTest {
-    
+
     private static final String NAME = "prop1";
 
     @Test
@@ -56,9 +56,15 @@ public class JcrXmlValueConverterTest {
 
     @Test
     public void testStringArray() {
-        assertArrayEquals(new Object[] { "myString1", "myString2" }, (Object[]) parseValue(NAME, "[myString1,myString2]"));
-        assertArrayEquals(new Object[] { "myString1,[]\\äöüß€", "myString2", "myString3 [ ] { } \\ ,", "", "[myString5]", "{myString6}" },
-                (Object[]) parseValue(NAME, "[myString1\\,[]\\\\äöüß€,myString2,myString3 [ ] { } \\\\ \\,,,[myString5],{myString6}]"));
+        assertArrayEquals(
+                new Object[] {"myString1", "myString2"}, (Object[]) parseValue(NAME, "[myString1,myString2]"));
+        assertArrayEquals(
+                new Object[] {
+                    "myString1,[]\\äöüß€", "myString2", "myString3 [ ] { } \\ ,", "", "[myString5]", "{myString6}"
+                },
+                (Object[]) parseValue(
+                        NAME,
+                        "[myString1\\,[]\\\\äöüß€,myString2,myString3 [ ] { } \\\\ \\,,,[myString5],{myString6}]"));
     }
 
     @Test
@@ -69,7 +75,7 @@ public class JcrXmlValueConverterTest {
 
     @Test
     public void testBooleanArray() {
-        assertArrayEquals(new Object[] { true, false }, (Object[]) parseValue(NAME, "{Boolean}[true,false]"));
+        assertArrayEquals(new Object[] {true, false}, (Object[]) parseValue(NAME, "{Boolean}[true,false]"));
     }
 
     @Test
@@ -80,8 +86,9 @@ public class JcrXmlValueConverterTest {
 
     @Test
     public void testLongArray() {
-        assertArrayEquals(new Object[] { 1L, 2L }, (Object[]) parseValue(NAME, "{Long}[1,2]"));
-        assertArrayEquals(new Object[] { 10000000000L, 20000000000L }, (Object[]) parseValue(NAME, "{Long}[10000000000,20000000000]"));
+        assertArrayEquals(new Object[] {1L, 2L}, (Object[]) parseValue(NAME, "{Long}[1,2]"));
+        assertArrayEquals(new Object[] {10000000000L, 20000000000L}, (Object[])
+                parseValue(NAME, "{Long}[10000000000,20000000000]"));
     }
 
     @Test
@@ -91,12 +98,13 @@ public class JcrXmlValueConverterTest {
 
     @Test
     public void testDoubleArray() {
-        assertArrayEquals(new Object[] { new BigDecimal("1.234"), new BigDecimal("2.345") }, (Object[]) parseValue(NAME, "{Decimal}[1.234,2.345]"));
+        assertArrayEquals(new Object[] {new BigDecimal("1.234"), new BigDecimal("2.345")}, (Object[])
+                parseValue(NAME, "{Decimal}[1.234,2.345]"));
     }
 
     @Test
     public void testCalendar() {
-        Calendar value = (Calendar)parseValue(NAME, "{Date}2010-09-05T15:10:20.000Z");
+        Calendar value = (Calendar) parseValue(NAME, "{Date}2010-09-05T15:10:20.000Z");
         assertEquals(2010, value.get(Calendar.YEAR));
         assertEquals(8, value.get(Calendar.MONTH));
         assertEquals(5, value.get(Calendar.DAY_OF_MONTH));
@@ -104,20 +112,20 @@ public class JcrXmlValueConverterTest {
 
     @Test
     public void testStringArrayRepPrivileges() {
-        assertArrayEquals(new Object[] { "rep:write", "crx:replicate", "jcr:read" }, (Object[]) parseValue(NAME, "{Name}[rep:write,crx:replicate,jcr:read]"));
+        assertArrayEquals(new Object[] {"rep:write", "crx:replicate", "jcr:read"}, (Object[])
+                parseValue(NAME, "{Name}[rep:write,crx:replicate,jcr:read]"));
     }
 
     @Test
     public void testReference() {
         UUID uuid = UUID.randomUUID();
-        UUID value = (UUID)parseValue(NAME, "{Reference}" + uuid.toString());
+        UUID value = (UUID) parseValue(NAME, "{Reference}" + uuid.toString());
         assertEquals(uuid, value);
     }
 
     @Test
     public void testURI() {
-        URI value = (URI)parseValue(NAME, "{URI}http://www.jodelkaiser.de/");
+        URI value = (URI) parseValue(NAME, "{URI}http://www.jodelkaiser.de/");
         assertEquals("http://www.jodelkaiser.de/", value.toString());
     }
-
 }
